7cab7edbdad36380d8189103018d295976548af1
acd1034 <48613285+acd1034@users.noreply.github.com>
2022-09-13 11:05:40 +0900

    🚚 Rename iris→namedargs

--- tests/main/main.cpp
+++ tests/main/main.cpp
@@ -1,7 +1,7 @@
 #include <catch2/catch_test_macros.hpp>
-#include <iris/iris.hpp>
+#include <namedargs/fundamental.hpp>
 
 TEST_CASE("main", "[main][squared]") {
-  static_assert(std::is_same_v<decltype(iris::squared(0)), int>);
-  CHECK(iris::squared(2) == 4);
+  static_assert(std::is_same_v<decltype(namedargs::squared(0)), int>);
+  CHECK(namedargs::squared(2) == 4);
 }

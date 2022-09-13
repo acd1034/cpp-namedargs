58fb63e56d95e50688cee0a017eaabde8293516e
acd1034 <48613285+acd1034@users.noreply.github.com>
2022-09-13 11:10:06 +0900

    ✨ Add similar_to

--- include/namedargs/fundamental.hpp
+++ include/namedargs/fundamental.hpp
@@ -1,8 +1,8 @@
 /// @file fundamental.hpp
 #pragma once
 #include <cassert>
-// #include <compare>
-// #include <concepts>
+#include <compare>
+#include <concepts>
 #include <cstddef> // std::size_t, std::ptrdiff_t, std::nullptr_t
 #include <cstdint> // std::int32_t
 #include <initializer_list>
@@ -16,4 +16,8 @@ namespace namedargs {
   constexpr auto squared(const X& x) noexcept(noexcept(x* x)) -> decltype(x * x) {
     return x * x;
   }
+
+  /// similar_to
+  template <class T, class U>
+  concept similar_to = std::is_same_v<std::remove_cvref_t<T>, std::remove_cvref_t<U>>;
 } // namespace namedargs
